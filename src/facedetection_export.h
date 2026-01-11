fdc006a43023dbc795b63bf8cb77e1fe121e0b6b
Phong Pham <phongpham663@gmail.com>
2026-01-11 13:19:26 +0700

    update code

---
+++ src/facedetection_export.h
@@ -0,0 +1,11 @@
+/*
+ * facedetection_export.h
+ * Define FACEDETECTION_EXPORT for static compilation
+ */
+
+#ifndef FACEDETECTION_EXPORT_H
+#define FACEDETECTION_EXPORT_H
+
+#define FACEDETECTION_EXPORT
+
+#endif // FACEDETECTION_EXPORT_H
